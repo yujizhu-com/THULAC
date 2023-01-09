11182c75c9f59af66eb14ff44e4c73500e0aeab5
余纪柱 <1440198508@qq.com>
2023-01-09 18:50:11 +0800

    初始化
    移除无用文件
    
    xcode 工程初始化
    
    xcode 用户状态文件忽略
    
    移除大文件夹

---
+++ thulac/thulac/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  thulac
+//
+//  Created by 余纪柱 on 2023/1/9.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
